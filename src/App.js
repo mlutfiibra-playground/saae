26cfe1441e3c66dba66e5e6b27ca850d703b3143
mlutfiibra <mlutfiibra@outlook.com>
2019-12-25 20:57:38 +0700

    Fix some config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 
 import Location from 'components/location';
 import Weather from 'components/weather';
+
+import './style.scss';
 
 function App() {
   const [currentLocation, setCurrentLocation] = useState('');
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <p>Saae OS</p>
         <Location handleCurrentLocation={setCurrentLocation} />
         <Weather currentLocation={currentLocation} />
-      </header>
+      </div>
     </div>
   );
 }
