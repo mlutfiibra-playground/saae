011fec7836a5f92d9cd56def1c1bb613650dc7cf
mlutfiibra <mlutfiibra@outlook.com>
2019-12-29 23:28:24 +0700

    Rekognition Controller

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  endOfLine: 'auto',
-  jsxBracketSameLine: true,
-  jsxSingleQuote: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-};
--- src/services/WeatherService.js
+++
@@ -1,9 +0,0 @@
-import MY_AXIOS from './index';
-
-export default {
-  getWeather(city) {
-    return MY_AXIOS.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4ff4e2fa6c87cdf3c077e6eb3b9bc719`
-    );
-  },
-};
