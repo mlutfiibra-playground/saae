59d2e6fb406846cbc07e63dc812225fd5cf3dc58
mlutfiibra <mlutfiibra@outlook.com>
2019-12-30 02:18:44 +0700

    Config

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = require("./expressInstance");
+const io = require("socket.io")(http);
+
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const index = require("./router");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(function(req, res, next) {
+  req.io = io;
+  next();
+});
+app.use("/", index);
+
+io.on("connection", function(socket) {
+  console.log("a user is connected");
+  io.emit("connect");
+});
+
+http.listen(port, () => {
+  console.log("listening to port", port);
+});
+
+//start-rubhi-123abc
+
+module.exports = app;
