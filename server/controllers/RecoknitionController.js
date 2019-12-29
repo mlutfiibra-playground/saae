40a5e55f76e4e495ef50e4660e24408a8339a70b
mlutfiibra <mlutfiibra@outlook.com>
2019-12-30 00:19:38 +0700

    Reinviting Amazon Rekognition

--- server/controllers/RecoknitionController.js
+++ server/controllers/RecoknitionController.js
@@ -1,36 +1,55 @@
-// Load the SDK and UUID
+const image2base64 = require("image-to-base64");
 var AWS = require("aws-sdk");
-var uuid = require("uuid");
+var keys = require(__dirname + "/amazonCredential.json");
+AWS.config.update(keys);
+var rekognition = new AWS.Rekognition({ apiVersion: "2016-06-27" });
 
-// Create unique bucket name
-var bucketName = "node-sdk-sample-" + uuid.v4();
-// Create name for uploaded object key
-var keyName = "hello_world.txt";
-
-// Create a promise on S3 service object
-var bucketPromise = new AWS.S3({ apiVersion: "2006-03-01" })
-  .createBucket({ Bucket: bucketName })
-  .promise();
-
-// Handle promise fulfilled/rejected states
-bucketPromise
-  .then(function(data) {
-    // Create params for putObject call
-    var objectParams = {
-      Bucket: bucketName,
-      Key: keyName,
-      Body: "Hello World!"
+const faceDetection = async imageLink => {
+  try {
+    console.log({ imageLink });
+    let base64 = await image2base64(imageLink);
+    const buffer = new Buffer.from(base64, "base64");
+    var params = {
+      Image: {
+        /* required */
+        Bytes: buffer
+      },
+      Attributes: ["ALL"]
     };
-    // Create object upload promise
-    var uploadPromise = new AWS.S3({ apiVersion: "2006-03-01" })
-      .putObject(objectParams)
-      .promise();
-    uploadPromise.then(function(data) {
-      console.log(
-        "Successfully uploaded data to " + bucketName + "/" + keyName
-      );
+
+    return new Promise(function(resolve, reject) {
+      rekognition.detectFaces(params, function(err, data) {
+        if (err) reject(err);
+        // an error occurred
+        else resolve(data); // successful response
+      });
     });
-  })
-  .catch(function(err) {
-    console.error(err, err.stack);
-  });
+  } catch (e) {
+    throw e;
+  }
+};
+
+const labelDetection = async imageLink => {
+  try {
+    let base64 = await image2base64(imageLink);
+    const buffer = new Buffer.from(base64, "base64");
+    var params = {
+      Image: {
+        /* required */
+        Bytes: buffer
+      }
+    };
+
+    return new Promise(function(resolve, reject) {
+      rekognition.detectLabels(params, function(err, data) {
+        if (err) reject(err);
+        // an error occurred
+        else resolve(data); // successful response
+      });
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports = { faceDetection, labelDetection };
