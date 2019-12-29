cc6112201ebcb1b12a254dee4bf24d23779c0077
mlutfiibra <mlutfiibra@outlook.com>
2019-12-30 02:26:08 +0700

    IBM Watson config

---
+++ server/controllers/IBMWatsonController.js
@@ -0,0 +1,36 @@
+const PersonalityInsightsV3 = require("ibm-watson/personality-insights/v3");
+require("dotenv").config();
+const iam_apikey = process.env.IBM_API_KEY;
+const converDataStructure = require("../helper/dataToObjectCreator");
+
+const personalityInsights = new PersonalityInsightsV3({
+  version: "2017-10-13",
+  iam_apikey
+});
+
+module.exports = function(data) {
+  console.log(data, "!!!!!!!!!!!!!!!");
+  let { IBMAnalysis } = converDataStructure(data);
+  const profileParams = {
+    content: JSON.parse(JSON.stringify(IBMAnalysis)),
+    content_type: "application/json",
+    consumption_preferences: true,
+    raw_scores: true
+  };
+  // return personalityInsights.profile(profileParams)
+  console.log(profileParams.content.contentItems, "!!!!!!!!!!!!!!!!!");
+  return new Promise((resolve, reject) => {
+    personalityInsights
+      .profile(profileParams)
+      .then(response => {
+        resolve(response);
+      })
+      .catch(err => {
+        if (err.code === 400) {
+          resolve({ error: err.message });
+        } else {
+          throw err;
+        }
+      });
+  });
+};
